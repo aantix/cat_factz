6b7b211633b09bed66e3a2b5e1684a23abe1548c
Jim Jones <jjones@aantix.com>
2012-05-05 17:56:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatFactz::Application.config.secret_token = 'ce7411775a51157106c05fe0ad46ccefdc1cad34a4f226dc6c7060545c053fc5e1aa06f62e1612fda5caefcee2eccab42aaa3194d6a241db0daef6b63da280df'
